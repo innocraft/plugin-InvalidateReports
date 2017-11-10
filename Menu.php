40c815e4f6bcec6fbee64c217b6abc8e511f747f
sgiehl <stefan@piwik.org>
2017-11-10 21:03:10 +0100

    Adds menu entry to admin

---
+++ Menu.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\InvalidateReports;
+
+use Piwik\Menu\MenuAdmin;
+use Piwik\Piwik;
+
+class Menu extends \Piwik\Plugin\Menu
+{
+    public function configureAdminMenu(MenuAdmin $menu)
+    {
+        if (Piwik::hasUserSuperUserAccess()) {
+            $menu->addSystemItem(
+                'InvalidateReports_InvalidateReports',
+                $this->urlForAction('index'),
+                $order = 10
+            );
+        }
+    }
+
+}
